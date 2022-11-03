0e1237c3ef30686b6de963a5e8d9615c5dd93334
kane04036 <hi011004@naver.com>
2022-11-04 02:57:24 +0900

    code

---
+++ 1152.swift
@@ -0,0 +1,15 @@
+//
+//  1152.swift
+//  BaekJoon_Swift
+//
+//  Created by 제임스 on 2022/11/04.
+//
+
+//import Foundation
+//
+//var line = readLine()!
+//var array : Array<String> = line.components(separatedBy: " ")
+//
+//array.removeAll(where: {$0 == ""})
+//print(array.count)
+//
